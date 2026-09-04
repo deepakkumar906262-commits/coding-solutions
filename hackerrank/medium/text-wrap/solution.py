455ec429f26d8de08612a34686b374b3c2f260ac
Deepak Kumar <deepakkumar906262@gmail.com>
2026-09-05 01:11:42 +0530

    ✅ Text Alignment | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-04

--- hackerrank/medium/text-wrap/solution.py
+++ hackerrank/medium/text-wrap/solution.py
@@ -1,5 +1,22 @@
+thickness = int(input()) #This must be an odd number
+c = 'H'
 
+#Top Cone
+for i in range(thickness):
+    print((c*i).rjust(thickness-1)+c+(c*i).ljust(thickness-1))
 
-def wrap(string, max_width):
-    return "\n".join(textwrap.wrap(string, max_width))
+#Top Pillars
+for i in range(thickness+1):
+    print((c*thickness).center(thickness*2)+(c*thickness).center(thickness*6))
 
+#Middle Belt
+for i in range((thickness+1)//2):
+    print((c*thickness*5).center(thickness*6))    
+
+#Bottom Pillars
+for i in range(thickness+1):
+    print((c*thickness).center(thickness*2)+(c*thickness).center(thickness*6))    
+
+#Bottom Cone
+for i in range(thickness):
+    print(((c*(thickness-i-1)).rjust(thickness)+c+(c*(thickness-i-1)).ljust(thickness)).rjust(thickness*6))
