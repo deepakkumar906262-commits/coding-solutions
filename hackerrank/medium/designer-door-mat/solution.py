adf91094410fb51b784cdc1ecfd230f8d73fd7bd
Deepak Kumar <deepakkumar906262@gmail.com>
2026-09-05 01:13:43 +0530

    ✅ Text Wrap | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-04

--- hackerrank/medium/designer-door-mat/solution.py
+++ hackerrank/medium/designer-door-mat/solution.py
@@ -1,13 +1,5 @@
-# Read space-separated values of N and M
-n, m = map(int, input().split())
 
-# Top half of the door mat
-for i in range(1, n, 2):
-    print((".|." * i).center(m, '-'))
 
-# Welcome line in the center
-print("WELCOME".center(m, '-'))
+def wrap(string, max_width):
+    return "\n".join(textwrap.wrap(string, max_width))
 
-# Bottom half of the door mat (reverse of the top half)
-for i in range(n - 2, 0, -2):
-    print((".|." * i).center(m, '-'))
